bdd76435c67bc38981e6333c5204faac81c2a860
MarlonDB-003 <marlondeividmd@gmail.com>
2023-09-25 11:00:10 -0400

    Atualizações projeto Scentia

--- main.js
+++ main.js
@@ -3,94 +3,94 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
 }
 
 /* QUANDO CLICAR EM UM ITEM DO MENU,ESCODER O MENU */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
 }
 
 /* mudar oheader da pagina quando der scroll */
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeigth = header.offsetHeigth
+    const header = document.querySelector('#header')
+    const navHeigth = header.offsetHeigth
 
-  if (window.scrollY >= navHeigth) {
-    // scroll é maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    // menor que a altura do header
-    header.classList.remove('scroll')
-  }
+    if (window.scrollY >= navHeigth) {
+        // scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
 }
 
 /* ScrollReveal: mostrar elementos quando der scroll na pagina */
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
 })
 
 scrollReveal.reveal(
-  `#home .image, #home .text,
-  #about .image, #about .text,
-  #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
+    `#home .image, #home .text,
+  #engsoft .image, #engsoft .text,
+  #si .image, #si .text,
+  #cursos header, 
   #contact .text, #contact .links,
   footer .brand, footer .social
-  `,
-  { interval: 100 }
+  `, { interval: 100 }
 )
 
 /* Botão voltar para o topo */
 
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+    const backToTopButton = document.querySelector('.back-to-top')
 
-  if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
 }
 
 /* Menu ativo conforme a seção visível na página */
 const sections = document.querySelectorAll('main section[id]')
+
 function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
     }
-  }
 }
 /* WHEN SCROLL */
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection()
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
 })
